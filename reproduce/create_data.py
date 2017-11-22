9b59fbce11f570c0d3242097e01685d44ded452f
Qinka <qinka@live.com>
2017-11-22 21:33:47 +0800

    update

--- reproduce/create_data.py
+++ reproduce/create_data.py
@@ -12,7 +12,9 @@ sys.path.append('../py/')
 
 import pyoctnet
 
-def create_date_with_model(vx_res = 64, out_root = './preprocessed',mn10_path = 'mn10.zip', pad=2, n_rots = 1, n_processes = 10, n_threads = 1, seed_num = 42):
+def create_data_with_model(vx_res = 64,mn10_path = 'mn10.zip', pad=2, n_rots = 1, n_processes = 10, n_threads = 1, seed_num = 42):
+
+  out_root = './preprocessed/r' + str(vx_res)
 
   random.seed(seed_num)
   np.random.seed(seed_num)
