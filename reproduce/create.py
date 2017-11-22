c73eefe9b1c7fcc145918e4f79edd050237881be
Qinka <qinka@live.com>
2017-11-22 22:26:32 +0800

    update

--- reproduce/create.py
+++ reproduce/create.py
@@ -3,4 +3,4 @@
 import create_data as c
 
 if __name__ == '__main__':
-  c.create_data(vx_res = 8, mn10_path='../example/01_classification_modelnet/mn10.zip')
+  c.create_data_with_modelcreate_data(vx_res = 8, mn10_path='../example/01_classification_modelnet/mn10.zip')
