0b6e222ec5394854217668cf342e662f56eae930
Qinka <qinka@live.com>
2017-11-22 22:51:45 +0800

    update

--- reproduce/create_data.py
+++ reproduce/create_data.py
@@ -77,9 +77,9 @@ def create_data_with_model(vx_res = 64,mn10_path = 'mn10.zip', pad=2, n_rots = 1
   for off_idx, off_path in enumerate(off_paths):
     print('%d pool.apply_async' % off_idx)
     if n_processes > 1:
-      pool.apply_async(worker, args=(rot_idx, rot, off_idx, off_path,n_rots,off_path,))
+      pool.apply_async(worker, args=(rot_idx, rot, off_idx, off_path,n_rots,len(off_paths),))
     else:
-      worker(rot_idx, rot, off_idx, off_path,n_rots,off_path)
+      worker(rot_idx, rot, off_idx, off_path,n_rots,len(off_paths))
 
   if n_processes > 1:
     pool.close()
@@ -89,8 +89,8 @@ def create_data_with_model(vx_res = 64,mn10_path = 'mn10.zip', pad=2, n_rots = 1
 
 
 # create grid-octree from off mesh
-def worker(rot_idx, rot, off_idx, off_path,n_rots,off_path):
-  print('%d/%d - %d/%d - %s' % (rot_idx+1, n_rots, off_idx+1, len(off_paths), off_path))
+def worker(rot_idx, rot, off_idx, off_path,n_rots,opsl):
+  print('%d/%d - %d/%d - %s' % (rot_idx+1, n_rots, off_idx+1, opsl, off_path))
 
   phi = rot / 180.0 * np.pi
   R = np.array([
