e1baebca03fcb95b117dd2e36ea0e2223bcf75c4
Johann Lee <qinka@live.com>
2017-12-22 09:02:08 +0800

    update

---
+++ reproduce/rep_train2.lua
@@ -0,0 +1,97 @@
+local on1_mn10_r8   = dofile('on1_mn10_r8.lua')
+local on1_mn10_r16  = dofile('on1_mn10_r16.lua')
+local on1_mn10_r32  = dofile('on1_mn10_r32.lua')
+local on1_mn10_r64  = dofile('on1_mn10_r64.lua')
+local on1_mn10_r128 = dofile('on1_mn10_r128.lua')
+local on1_mn10_r256 = dofile('on1_mn10_r256.lua')
+
+local on2_mn10_r8   = dofile('on2_mn10_r8.lua')
+local on2_mn10_r16  = dofile('on2_mn10_r16.lua')
+local on2_mn10_r32  = dofile('on2_mn10_r32.lua')
+local on2_mn10_r64  = dofile('on2_mn10_r64.lua')
+local on2_mn10_r128 = dofile('on2_mn10_r128.lua')
+local on2_mn10_r256 = dofile('on2_mn10_r256.lua')
+
+local on3_mn10_r8   = dofile('on3_mn10_r8.lua')
+local on3_mn10_r16  = dofile('on3_mn10_r16.lua')
+local on3_mn10_r32  = dofile('on3_mn10_r32.lua')
+local on3_mn10_r64  = dofile('on3_mn10_r64.lua')
+local on3_mn10_r128 = dofile('on3_mn10_r128.lua')
+local on3_mn10_r256 = dofile('on3_mn10_r256.lua')
+
+
+on1_mn10_r8.train(4)
+on1_mn10_r8.train(32)
+on1_mn10_r8.train(128)
+
+on1_mn10_r16.train(4)
+on1_mn10_r16.train(32)
+on1_mn10_r16.train(128)
+
+on1_mn10_r32.train(4)
+on1_mn10_r32.train(32)
+on1_mn10_r32.train(128)
+
+on1_mn10_r64.train(4)
+on1_mn10_r64.train(32)
+on1_mn10_r64.train(128)
+
+on1_mn10_r128.train(4)
+on1_mn10_r128.train(32)
+on1_mn10_r128.train(128)
+
+on1_mn10_r256.train(4)
+on1_mn10_r256.train(32)
+on1_mn10_r256.train(128)
+
+on2_mn10_r8.train(4)
+on2_mn10_r8.train(32)
+on2_mn10_r8.train(128)
+
+on2_mn10_r16.train(4)
+on2_mn10_r16.train(32)
+on2_mn10_r16.train(128)
+
+on2_mn10_r32.train(4)
+on2_mn10_r32.train(32)
+on2_mn10_r32.train(128)
+
+on2_mn10_r64.train(4)
+on2_mn10_r64.train(32)
+on2_mn10_r64.train(128)
+
+on2_mn10_r128.train(4)
+on2_mn10_r128.train(32)
+on2_mn10_r128.train(128)
+
+on2_mn10_r256.train(4)
+on2_mn10_r256.train(32)
+on2_mn10_r256.train(128)
+
+on3_mn10_r8.train(4)
+on3_mn10_r8.train(32)
+on3_mn10_r8.train(128)
+
+on3_mn10_r16.train(4)
+on3_mn10_r16.train(32)
+on3_mn10_r16.train(128)
+
+on3_mn10_r32.train(4)
+on3_mn10_r32.train(32)
+on3_mn10_r32.train(128)
+
+on3_mn10_r64.train(4)
+on3_mn10_r64.train(32)
+on3_mn10_r64.train(128)
+
+on3_mn10_r128.train(4)
+on3_mn10_r128.train(32)
+on3_mn10_r128.train(128)
+
+on3_mn10_r256.train(4)
+on3_mn10_r256.train(32)
+on3_mn10_r256.train(128)
+
+
+
+
