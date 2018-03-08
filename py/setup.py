62e13c9bf7dfb08eaea2e7debd5998527dc6227d
Johann Lee <me@qinka.pro>
2018-03-08 10:36:57 +0800

    test

--- py/setup.py
+++ py/setup.py
@@ -29,6 +29,7 @@ from distutils.extension import Extension
 from Cython.Distutils import build_ext
 import numpy as np
 import platform
+import os
 
 extra_compile_args = ['-msse', '-msse2', '-msse3', '-msse4.2']
 extra_link_args = []
@@ -42,33 +43,14 @@ setup(
   cmdclass= {'build_ext': build_ext},
   ext_modules=[
     Extension('pyoctnet',
-      ['pyoctnet.pyx',
-       '../core/src/core.cpp',
-       '../core/src/d2o.cpp',
-       '../core/src/o2d.cpp',
-       '../core/src/io.cpp',
-       '../core/src/misc.cpp',
-       '../core/src/gridpool.cpp',
-       '../core/src/gridunpool.cpp',
-       '../core/src/conv.cpp',
-       '../core/src/combine.cpp',
-       '../core/src/split.cpp',
-       '../create/src/create.cpp',
-       '../create/src/create_dense.cpp',
-       '../create/src/create_mesh.cpp',
-       '../create/src/create_obj.cpp',
-       '../create/src/create_off.cpp',
-       '../create/src/create_pc.cpp',
-       '../create/src/utils.cpp',
-        ],
-      language='c++',
-      include_dirs=[np.get_include(),
-        '../core/include/',
-        '../create/include/',
-        '../geometry/include/',
-        ],
-      extra_compile_args=extra_compile_args,
-      extra_link_args=extra_link_args
+              ['pyoctnet.pyx'],
+              language='c++',
+              include_dirs=[np.get_include(),
+                            os.environ['OC_INC_DIR']
+              ],
+              libraries=['octnet'],
+              extra_compile_args=extra_compile_args,
+              extra_link_args=extra_link_args
     )
   ]
 )
