6f40a7cb262412e7f3048afc5e81009988933b0f
Johann Lee <me@qinka.pro>
2018-03-14 11:11:42 +0800

    update

--- oc/VisualOC.lua
+++ oc/VisualOC.lua
@@ -15,11 +15,30 @@ local VisualOCModel, parent = torch.class('oc.VisualOC', 'oc.OctreeModule')
 function VisualOCModel:__init()
     parent.__init()
     self.ok = nil;
+    self.video = torch.FloatTensor()
+    self.dense_depth = dense_depth
+    self.dense_height = dense_height
+    self.dense_width = dense_width
 end
 
+function VisualOCModel:dense_dimensions(octrees)
+    if self.dense_depth and self.dense_height and self.dense_width then
+      return self.dense_depth, self.dense_height, self.dense_width 
+    else
+      return octrees:dense_depth(), octrees:dense_height(), octrees:dense_width()
+    end
+  end 
 
 function  VisualOCModel:updateOutput(input)
     if first then
+        local dense_depth, dense_height, dense_width = self:dense_dimensions(input)
+        local out_size = torch.LongStorage({input:n(), input:feature_size(), dense_depth, dense_height, dense_width})
+        self.video:resize(out_size)
+        if input._type == 'oc_float' then
+            oc.cpu.octree_to_cdhw_cpu(input.grid, dense_depth, dense_height, dense_width, self.video:data())
+          elseif input._type == 'oc_cuda' then
+            oc.gpu.octree_to_cdhw_gpu(input.grid, dense_depth, dense_height, dense_width, self.video:data())
+          end
         self.ok = pcall(plot.video, plot.video, {tensor = video})
         if self.ok then
             print('Uploaded video')
