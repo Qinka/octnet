ab33cdc327fb49e5675b25fb3d9a42598d34f70e
Johann Lee <me@qinka.pro>
2018-04-22 16:20:08 +0800

    update

--- reproduce/create_from_pc.py
+++ reproduce/create_from_pc.py
@@ -52,13 +52,13 @@ def create_oc_frompc(vx_res=256,in_root='PartAnnotation', n_processes=1, n_threa
     
 def worker(outroot: str, filedata : str, vx_res, n_threads=1):
     try:
-        t,i = insert_into_pair(filedata)
+        k,i = insert_into_pair(filedata)
         prefix,item = os.path.split(filedata)
 
         kinds = os.listdir(prefix+'_label')
         print('read data', filedata)
         t   = time.time()
-        xyz = np.loadtxt(filedata[0],dtype=np.float32)
+        xyz = np.loadtxt(filedata,dtype=np.float32)
         print('\ttook %f[s]' % (time.time() - t))
         
         t = time.time()
@@ -72,18 +72,18 @@ def worker(outroot: str, filedata : str, vx_res, n_threads=1):
             else:
                 ns.append(np.zeros((xyz.shape[0],1)))
         plabel = np.concatenate(ns,axis=1) 
-        print('\ttook %f[s]' % (time.time() - t))
+        print('\ttook %f[s]' % (time.time() - t), plabel.shape)
         
         print('create octree')
         # object
-        grid  = pyoctnet.Octree.create_from_pc_simple(xyz,vx_res,vx_res,vx_res,False,n_threads=n_threads)
+        grid  = pyoctnet.Octree.create_from_pc_simple(xyz,1,vx_res,vx_res,vx_res,False,n_threads=n_threads)
         # part seg
-        label = pyoctnet.Octree.create_from_pc(xyz,plabel,vx_res,vx_res,vx_res,False,n_threads=n_threads)
+        label = pyoctnet.Octree.create_from_pc(xyz,plabel,  vx_res,vx_res,vx_res,False,n_threads=n_threads)
         print('\ttook %f[s]' % (time.time() - t))
         
         
-        oc_out_path = os.path.join(outroot, t, i + '_' + "pts.oc")
-        lb_out_path = os.path.join(outroot, t, i + '_' + "seg.oc")
+        oc_out_path = os.path.join(outroot, k, i + '_' + "pts.oc")
+        lb_out_path = os.path.join(outroot, k, i + '_' + "seg.oc")
         if not os.path.exists(os.path.split(oc_out_path)[0]):
             os.makedirs(os.path.split(oc_out_path)[0])
         t = time.time()
@@ -93,8 +93,11 @@ def worker(outroot: str, filedata : str, vx_res, n_threads=1):
         print('\ttook %f[s]' % (time.time() - t))
     except KeyboardInterrupt as e:
         raise e
-    except:
+    except Exception as e:
         print('error; breake')
+        print(e)
+        import traceback
+        traceback.print_exc()
         return
         
         
